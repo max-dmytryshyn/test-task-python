ec05a3719d5ad816204c7925b0bf41b89c127543
max-dmytryshyn <maksumus22@gmail.com>
2022-03-07 20:50:22 +0200

    Create Group and User models, serializers and viewsets

---
+++ Backend/test_task_python_backend/users_and_groups/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+
+router = routers.DefaultRouter()
+router.register(r'groups', views.GroupViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = router.urls
---
+++ Backend/test_task_python_backend/users_and_groups/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .models import Group, User
+from .serializers import GroupSerializer, UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
