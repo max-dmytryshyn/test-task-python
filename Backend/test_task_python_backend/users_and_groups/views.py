d56bf266909292344856ae75a252421349fe4cd2
max-dmytryshyn <maksumus22@gmail.com>
2022-03-09 23:22:13 +0200

    Add custom destroy method for Group

--- Backend/test_task_python_backend/users_and_groups/views.py
+++ Backend/test_task_python_backend/users_and_groups/views.py
@@ -1,4 +1,8 @@
+from django.db.models import ProtectedError
+from django.http import HttpResponse
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+
 from .models import Group, User
 from .serializers import GroupSerializer, UserSerializer
 
@@ -6,6 +10,15 @@ from .serializers import GroupSerializer, UserSerializer
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+    def destroy(self, request, pk=None):
+        queryset = Group.objects.all()
+        group = get_object_or_404(queryset, pk=pk)
+        try:
+            group.delete()
+        except ProtectedError:
+            return HttpResponse(status=403,
+                                reason="Forbidden. Can't delete this group because there is at least one user in it")
 
 
 class UserViewSet(viewsets.ModelViewSet):
