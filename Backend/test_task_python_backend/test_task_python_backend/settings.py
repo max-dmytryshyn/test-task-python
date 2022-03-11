0b3e9bdc32e9799edc9b0910a5008a9170487646
max-dmytryshyn <maksumus22@gmail.com>
2022-03-11 22:16:46 +0200

    Containerize backend Django app + PostgreSQL DB

--- Backend/test_task_python_backend/test_task_python_backend/settings.py
+++ Backend/test_task_python_backend/test_task_python_backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -75,7 +75,7 @@ DATABASES = {
         'NAME': os.getenv('NAME'),
         'USER': os.getenv('USERNAME'),
         'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
